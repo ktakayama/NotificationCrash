5ae19d08549aeba034b3c341a8a0b002527b543d
Kyosuke Takayama <loiseau@gmail.com>
2017-12-02 09:39:35 +0900

    bug

--- NotificationCrash/ViewController.swift
+++ NotificationCrash/ViewController.swift
@@ -6,19 +6,37 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { granted, error in
+            guard error == nil else { return }
 
+            DispatchQueue.main.async {
+                UIApplication.shared.cancelAllLocalNotifications()
+
+                let calendar = Calendar(identifier: .gregorian)
+                let timeZone = TimeZone.autoupdatingCurrent
+
+                guard let date = DateComponents(calendar: calendar, timeZone: timeZone, year: 2017, month: 12, day: 1).date
+                    else { return }
+
+                let notify = UILocalNotification()
+                notify.alertBody = "alert";
+                notify.alertAction = "view"
+                notify.repeatInterval = .month
+                notify.fireDate = date
+
+                UIApplication.shared.scheduleLocalNotification(notify)
+                print(#line, ": ", notify)
+            }
+        }
+    }
 
 }
+
 
